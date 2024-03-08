6796709dd34c3cc4f12aa1f781e16c19770d7fc0
Gayathri Harikumar <101390505+gayathrihk175@users.noreply.github.com>
2024-03-08 14:46:23 +0530

    added

---
+++ src/useDimension.js
@@ -0,0 +1,16 @@
+import { useEffect,useState } from "react";
+
+const useDimension = () => {
+    const [dimension,setDimension] = useState({width:0,height:0});
+    const updateDimension = () => {
+        const {innerWidth,innerHeight} = window;
+        setDimension({width:innerWidth,height:innerHeight});
+    }
+    useEffect(() => {
+        updateDimension()
+        window.addEventListener("resize",updateDimension);
+        return () => {window.addEventListener("resize",updateDimension)}
+    },[])
+    return dimension;
+}
+export default useDimension;
